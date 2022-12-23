7a00325b68cc65cccd51d4ba11184e3a153a347b
Marc Hamilton <unionhills@cox.net>
2022-12-22 18:04:26 -0700

    validates topping does not already exist upon creating a new one

--- pizza-app-blitzjs/src/toppings/mutations/createTopping.ts
+++ pizza-app-blitzjs/src/toppings/mutations/createTopping.ts
@@ -1,18 +1,22 @@
-import { resolver } from "@blitzjs/rpc";
-import db from "db";
-import { z } from "zod";
+import { resolver } from "@blitzjs/rpc"
+import db from "db"
+import { z } from "zod"
 
 const CreateTopping = z.object({
   name: z.string(),
-});
+})
 
 export default resolver.pipe(
   resolver.zod(CreateTopping),
-  resolver.authorize(),
+  //resolver.authorize(),
   async (input) => {
-    // TODO: in multi-tenant app, you must add validation to ensure correct tenant
-    const topping = await db.topping.create({ data: input });
+    const existingTopping = await db.topping.findFirst({ where: { name: input.name } })
 
-    return topping;
+    if (existingTopping) throw new Error(`Topping ${input.name} already exists`)
+
+    // TODO: in multi-tenant app, you must add validation to ensure correct tenant
+    const topping = await db.topping.create({ data: input })
+
+    return topping
   }
-);
+)
